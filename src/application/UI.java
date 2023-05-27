9279268c7d7ffc96a8608abda2c6ceb887740ade
Lucas Cavalcanti <lcavalcantidearaujo@gmail.com>
2023-05-26 22:57:14 -0300

    little improvement in board printing

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,9 +1,33 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
 
+	// códigos especiais das cores para se imprimir no console
+	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+	//cores do text
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	//cores do background
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+	
 	//metodo que vai imprimir o tabuleiro
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i=0; i<pieces.length; i++) {
@@ -17,13 +41,18 @@ public class UI {
 	}
 	
 	//metodo que vai imprimir uma unica peça:
-	public static void printPiece(ChessPiece piece) {
-		if (piece == null) { //se essa peça for = nulo, significa que não tinha peça nessa posição do tabuleiro
-			System.out.print("-");
-		} 
-		else { //caso contrário, vai imprimir a peça que estava naquela posição
-			System.out.print(piece);
-		}	
-		System.out.print(" "); //imprimir um espaço em branco para que as peças não fiquem grudadas
+	private static void printPiece(ChessPiece piece) {
+    	if (piece == null) { //se essa peça for = nulo, significa que não tinha peça nessa posição do tabuleiro
+            System.out.print("-");
+        }
+        else { //caso contrário, vai imprimir a peça que estava naquela posição
+            if (piece.getColor() == Color.WHITE) {
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET); //o ANSI_RESET serve para resetar a cor
+            }
+            else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
+        }
+        System.out.print(" "); //imprimir um espaço em branco para que as peças não fiquem grudadas
 	}
 }
